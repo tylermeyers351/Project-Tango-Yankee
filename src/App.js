887254f2455510e7d61534fab0b2966c65c95bc2
tylermeyers351 <tylermeyers351@gmail.com>
2023-11-17 11:39:05 -0800

    Ready for API's and new component build out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/Header'
+import Sidebar from './components/Sidebar'
 import Start from './components/Start'
 import React from "react"
 
 
 function App() {
-  const [truthyCheck, setTruthyCheck] = React.useState(true)
+  // If there is data in local Storage (localData), it will start the truthyCheck as false. 
+  // True displays the <Start /> component,
+  // and false displays the <Sidebar />, <Planner />, and <Map /> components.
+  const [truthyCheck, setTruthyCheck] = React.useState(!localStorage.getItem('localData'))
 
   function updateTruthy() {
     setTruthyCheck(prevState => !prevState)
@@ -12,7 +16,6 @@ function App() {
   }
 
   const vacayData = JSON.parse(localStorage.getItem("localData"))
-  console.log(vacayData)
 
   return (
     <div className='container-fluid'>
@@ -41,14 +44,15 @@ function App() {
       <div className='row mainContent bg-light'>
           <div className="col-1 col-md-3 col-xl-4" >
             <p>Column 1 (Sidebar)</p>
+            <Sidebar updateTruthy={updateTruthy} truthyCheck={truthyCheck}/>
           </div>
           <div className="col-10 col-md-6 col-xl-4">
             <p>Column 2 (Planner - main content)</p>
             {vacayData && 
             <>
-            <p>Start Date: {vacayData.startDate}</p>
-            <p>End Date: {vacayData.endDate}</p>
-            <p>Location: {vacayData.location}</p>
+              <p>Start Date: {vacayData.startDate}</p>
+              <p>End Date: {vacayData.endDate}</p>
+              <p>Location: {vacayData.location}</p>
             </>
             }
           </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,19 @@
 import React from "react"
 
 function Sidebar(props) {
-    const hello = "hello"
+    const handleSubmit = () => {
+        props.updateTruthy()
+      };
+
     return (
         <div>
-
+            <div style={{display:"flex", justifyContent: 'center', alignItems:'center'}}>
+                <button 
+                    style={{color: 'white', backgroundColor: '#F5793B', fontWeight: 'bold', border: 'none'}} 
+                    onClick={handleSubmit} className="btn" type="button">
+                    New Trip!
+                </button>
+            </div>
         </div>
     );
 }
